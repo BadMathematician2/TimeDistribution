115206c2d6e3e44c1a5fa70cf08fd014e30aba7c
Mykola <mathematic204chnu@gmail.com>
2020-08-06 14:24:21 +0300

    уточнення

--- src/TimeDistribution.php
+++ src/TimeDistribution.php
@@ -6,8 +6,6 @@ namespace TimeDistribution;
 
 class TimeDistribution
 {
-
-
     /**
      * @param int $quantity
      * @param int $t_min
@@ -15,15 +13,18 @@ class TimeDistribution
      * @param int $c
      * @return float[]
      */
-    public final function distributeTime($quantity, $t_min, $t_max, $c = 1){
+    public final function distributeTime($quantity, $t_min, $t_max, $c = 1)
+    {
         $free_time = $t_max - $t_min;
         $time = 0;
         $result = [];
+        
         for ($i = 0; $i <= $quantity; $i++){
-            $result[] = rand($free_time/$quantity,$free_time/2)/($quantity*$c);
+            $result[] = rand($free_time / $quantity, $free_time / 2) / ($quantity * $c);
             $time += $result[$i];
         }
         $leftovers = ($free_time - $time)/($quantity+1);
+
         for ($i = 0; $i <= $quantity; $i++){
             $result[$i] += $leftovers;
         }
